5f90867e0fba25edd5b2758252577b05957405e3
KrunalG2909 <krunalg@testvagrant.com>
2020-06-17 19:21:38 +0530

    [Krunal] Added hello world scenario

---
+++ src/test/java/DockerTest.java
@@ -0,0 +1,10 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DockerTest {
+
+    @Test
+    public void jenkinsDockerTest(){
+        Reporter.log("Hello World!!!", true);
+    }
+}
